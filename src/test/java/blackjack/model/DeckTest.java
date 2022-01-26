0430dae41253eed42ca861dcee4db9df25f98aca
jiwoo <sgo8308@naver.com>
2022-01-26 22:55:54 +0900

    test : create DeckTest and add test of drawAndRemoveCardFromDeack()
    Test case
     - draw one card and check if deck has 51 card

---
+++ src/test/java/blackjack/model/DeckTest.java
@@ -0,0 +1,18 @@
+package blackjack.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DeckTest {
+
+    @Test
+    void draw_덱에서_카드가_한_장_줄어든다() {
+        //when
+        Card card = Deck.drawAndRemoveCardFromDeck();
+        boolean isSizeDown = Deck.isSize(51);
+        //then
+        Assertions.assertThat(isSizeDown).isTrue();
+    }
+}
