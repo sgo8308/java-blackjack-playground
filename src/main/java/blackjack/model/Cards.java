36dc06a8d58d41e260eb9852fcb93bc71e03706d
jiwoo <sgo8308@naver.com>
2022-01-26 22:50:55 +0900

    feat : create Cards and implements methods
    Methods
     - getSum()
     - getSumAfterHandkeA()
     - countCardA()

---
+++ src/main/java/blackjack/model/Cards.java
@@ -0,0 +1,37 @@
+package blackjack.model;
+
+import java.util.List;
+
+public class Cards {
+
+    List<Card> cards;
+
+    public Cards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public int getSum() {
+        int sumBeforeHandleA = cards.stream()
+                .map(card -> card.getNumber())
+                .mapToInt(CardNumber::getValue)
+                .sum();
+
+        return getSumAfterHandleA(sumBeforeHandleA);
+    }
+
+    private int getSumAfterHandleA(int sum) {
+        long cardACount = countCardA();
+        while (cardACount-- > 0 && sum > 21) {
+            sum -= CardNumber.DIFFERENCE_BETWEEN_BIG_A_AND_SMALL_A;
+        }
+        return sum;
+    }
+
+    private long countCardA() {
+        return cards.stream()
+                .map(card -> card.getNumber())
+                .filter(cardNumber -> cardNumber.equals(CardNumber.A))
+                .count();
+
+    }
+}
