df36181193f61da300742ed1bfc625ef757c610a
jiwoo <sgo8308@naver.com>
2022-01-26 22:48:30 +0900

    feat : create Card and add constructor and getter of cardNumber

---
+++ src/main/java/blackjack/model/Card.java
@@ -0,0 +1,16 @@
+package blackjack.model;
+
+public class Card {
+
+    private final CardType cardType;
+    private final CardNumber cardNumber;
+
+    public Card(CardType cardType, CardNumber cardNumber) {
+        this.cardType = cardType;
+        this.cardNumber = cardNumber;
+    }
+
+    public CardNumber getNumber() {
+        return cardNumber;
+    }
+}
