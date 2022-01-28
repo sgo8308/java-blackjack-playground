0ce296aa15e2acc8a6330f63eab165de5af0fcbb
jiwoo <sgo8308@naver.com>
2022-01-28 14:36:28 +0900

    feat : add name to CardNumber and modify constructor and add getter of name

--- src/main/java/blackjack/model/CardNumber.java
+++ src/main/java/blackjack/model/CardNumber.java
@@ -1,17 +1,27 @@
 package blackjack.model;
 
 public enum CardNumber {
-    A(11), ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), JACK(10), QUEEN(10), KING(10);
+    A(11, "A"), ONE(1, "1"), TWO(2, "2"),
+    THREE(3, "3"), FOUR(4, "4"), FIVE(5, "5"),
+    SIX(6, "6"), SEVEN(7, "7"), EIGHT(8, "8"),
+    NINE(9, "9"), JACK(10, "J"), QUEEN(10, "Q"),
+    KING(10, "K");
 
     public static int DIFFERENCE_BETWEEN_BIG_A_AND_SMALL_A = 10;
 
     private final int value;
+    private final String name;
 
-    CardNumber(int value) {
+    CardNumber(int value, String name) {
         this.value = value;
+        this.name = name;
     }
 
     public int getValue() {
         return value;
+    }
+
+    public String getName() {
+        return name;
     }
 }
