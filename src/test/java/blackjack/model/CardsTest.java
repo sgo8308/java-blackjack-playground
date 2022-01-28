4faee5e353285fd7ab061fe2a2ac201805cff396
jiwoo <sgo8308@naver.com>
2022-01-28 14:38:45 +0900

    test : rename getCards() to getCardsOnlyCLOVER() and add test of toString() to CardsTest

--- src/test/java/blackjack/model/CardsTest.java
+++ src/test/java/blackjack/model/CardsTest.java
@@ -1,18 +1,15 @@
 package blackjack.model;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class CardsTest {
     @Test
     void getSum_모든_카드의_값을_더한다() {
         //given
-        Cards cards = getCards(CardNumber.TWO, CardNumber.THREE, CardNumber.FOUR);
+        Cards cards = getCardsOnlyCLOVER(CardNumber.TWO, CardNumber.THREE, CardNumber.FOUR);
 
         //when
         int actualSum = cards.getSum();
@@ -24,8 +21,8 @@ class CardsTest {
     @Test
     void getSum_A가_들어있을_때는_더했을_때_21이_넘는다면_1로_치환해서_계산() {
         //given
-        Cards cardsWith1A = getCards(CardNumber.A, CardNumber.NINE, CardNumber.JACK);
-        Cards cardsWith2A = getCards(CardNumber.A, CardNumber.A, CardNumber.JACK);
+        Cards cardsWith1A = getCardsOnlyCLOVER(CardNumber.A, CardNumber.NINE, CardNumber.JACK);
+        Cards cardsWith2A = getCardsOnlyCLOVER(CardNumber.A, CardNumber.A, CardNumber.JACK);
 
         //when
         int actualSum1A = cardsWith1A.getSum();
@@ -36,7 +33,19 @@ class CardsTest {
         assertThat(actualSum2A).isEqualTo(12);
     }
 
-    private Cards getCards(CardNumber num1, CardNumber num2, CardNumber num3) {
+    @Test
+    void toString_마지막_카드_빼고_쉼표_붙힌_후_반환() {
+        //given
+        Cards cards = getCardsOnlyCLOVER(CardNumber.A, CardNumber.NINE, CardNumber.JACK);
+
+        //when
+        String cardsString = cards.toString();
+
+        //then
+        assertThat(cardsString).hasToString("A클로버, 9클로버, J클로버");
+    }
+
+    private Cards getCardsOnlyCLOVER(CardNumber num1, CardNumber num2, CardNumber num3) {
         Cards cards = new Cards(Arrays.asList(
                 new Card(CardType.CLOVER, num1),
                 new Card(CardType.CLOVER, num2),
@@ -44,4 +53,5 @@ class CardsTest {
         ));
         return cards;
     }
+
 }
