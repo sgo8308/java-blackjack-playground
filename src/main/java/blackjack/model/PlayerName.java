fd86c463897d153788a3be82fc3fd81b4aef532f
jiwoo <sgo8308@naver.com>
2022-02-05 18:47:22 +0900

    refactor : change name limit from 5 to 10 in Playername

--- src/main/java/blackjack/model/PlayerName.java
+++ src/main/java/blackjack/model/PlayerName.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 public class PlayerName {
 
-    private static final int LENGTH_LIMIT = 5;
+    private static final int LENGTH_LIMIT = 10;
     private final String name;
 
     public PlayerName(String name) {
@@ -15,6 +15,10 @@ public class PlayerName {
     private void validUnderLengthLimit(String name) {
         Optional<String> nameOpt = Optional.ofNullable(name);
         nameOpt.filter(s -> s.length() <= LENGTH_LIMIT)
-                .orElseThrow(() -> new IllegalArgumentException("이름은 5자를 초과하면 안됩니다."));
+                .orElseThrow(() -> new IllegalArgumentException("이름은 10자를 초과하면 안됩니다."));
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/test/java/blackjack/model/PlayerNameTest.java
+++ src/test/java/blackjack/model/PlayerNameTest.java
@@ -9,7 +9,8 @@ class PlayerNameTest {
     @Test
     void 플레이어_이름이_5자가_넘으면_예외_던지기() {
         Assertions.assertThatIllegalArgumentException().isThrownBy(() -> {
-            new PlayerName("fellojava");
-        }).withMessage("이름은 5자를 초과하면 안됩니다.");
+            new PlayerName("fellojavasdfs");
+        }).withMessage("이름은 10자를 초과하면 안됩니다.");
     }
 }
+
